bdba15166911c2eaaa9d7045a8fcf34502c0cb2d
bdonesm1111-wq <bdonesm1111@gmail.com>
2026-09-05 22:28:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -139,5 +139,9 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`App running on port ${PORT}`));
+app.use(express.static(__dirname));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
